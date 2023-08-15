3a6f79d967b27caafbcfc99d1d4a44da708c09a9
Igor Solovev <solovyov.spb@gmail.com>
2023-08-15 13:37:50 +0400

    buxfix: исправить порядок выполения роутов

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
+router.use(userRouter);
+router.use(cardRouter);
 router.use('*', (req, res) => {
   res
     .status(HTTP_STATUS_BAD_REQUEST)
     .send({ message: 'Обращение к несуществующему пути' });
 });
-router.use(userRouter);
-router.use(cardRouter);
 
 module.exports = router;
