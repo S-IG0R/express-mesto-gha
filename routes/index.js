872de033c734136f7054133f05fd62a71b6c3109
Igor Solovev <solovyov.spb@gmail.com>
2023-08-15 13:34:41 +0400

    buxfix: добаваить ответ для несуществующего роута

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
+const { HTTP_STATUS_BAD_REQUEST } = require('http2').constants;
+
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
+router.use('*', (req, res) => {
+  res
+    .status(HTTP_STATUS_BAD_REQUEST)
+    .send({ message: 'Обращение к несуществующему пути' });
+});
 router.use(userRouter);
 router.use(cardRouter);
 
